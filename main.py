12e46732e85b2811ab4ff0f54aeaffb5130a5fb9
AmishKakka <amishkakka@gmail.com>
2026-03-24 23:22:57 -0700

    Added a search box to search for companies

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from yfinance import ticker
 from agents import build_graph, graph_app
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from IPython.display import Markdown
+from setup_duck_db import fetchTickers
 
 app = FastAPI(title="Financial Agentic AI")
 
@@ -34,8 +36,25 @@ async def analyze(request: AnalysisRequest):
         return {
             "status": "error",
             "message": Markdown(str(e))
-        } 
-    
+        }
+
+
+@app.get("/suggestions")
+async def suggestions(q: str = ""):
+    tickers = fetchTickers()
+    companies = list(tickers["Symbol"])
+
+    if not q:
+        return {
+            "status": "success",
+            "data": companies
+        }
+    filtered = [cmp for cmp in companies if q.lower() in cmp.lower()]
+    return {
+        "status": "success",
+        "data": filtered
+    }
+
 
 @app.get("/")
 def home():
